974d56ad9eda0692f0116f826cb94e68307114c5
lihitarbes <mikilihi@gmail.com>
2024-11-11 17:33:15 +0200

    'hw'

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,24 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		
+int lin =Integer.parseInt(args[0]);
+int a = (int)((Math.random()) * (lin));
+int b = (int)(Math.random() * (lin));
+int c = (int)(Math.random() * (lin));
+
+System.out.println(a + " " + b + " " + c);
+
+int small = Math.min(a, b);
+int small1 = Math.min(small, c);
+int large = Math.max(a, b);
+int large1 = Math.max(large, c); 
+int middle = Math.max(small, c); 
+int middle1 = Math.min(middle, large);
+
+System.out.println(small1  + " " + middle1 + " " + large1);
 	}
-}
+	}
+
+
+
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,15 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	   
+ int numWord = Integer.parseInt(args[0]);
+ int ones = Math.floorMod(numWord,10);
+ int tens = Math.floorMod(numWord / 10,10);
+ int hundreds = Math.floorMod( numWord/ 100, 10);
+ 
+	System.out.println(hundreds + " hundreds " + tens + " tens " + "and " + ones + " ones ");
 	}
 }
+
+
+
