5155c8ad164e742c3f36974d5437e3187ebb3d6b
lihitarbes <mikilihi@gmail.com>
2024-11-13 23:43:08 +0200

    try9

--- NumWords.java
+++ NumWords.java
@@ -5,7 +5,7 @@ public class NumWords {
  int numWord = Integer.parseInt(args[0]);
  int ones = Math.floorMod(numWord,10);
  int tens = Math.floorMod(numWord / 10,10);
- int hundreds = Math.floorMod(numWord/ 100, 10);
+ int hundreds = (numWord/ 100);
  
 	System.out.println(hundreds + " hundreds" + ", " + tens +  " tens" + ", " + "and " + ones + " ones" + ".");
 	
